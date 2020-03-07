94de63aabbbbb064494bd6528b7099a75a250f45
Kevin Montero <kevinwilding1997@gmail.com>
2020-03-07 02:12:20 -0400

    menu fixed

--- src/component/Home/Navbar/Navbar.js
+++ src/component/Home/Navbar/Navbar.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 function Navbar() {
+    const [menuToggle, setMenuToggle] = useState(false);
+    console.clear()
+    console.log("menu btn's value:", menuToggle)
 	return (
 		<header className='Navbar'>
             <div className='Navbar-container container'>
                 <Link to='/'><p id='logo'><i className="line-logo fas fa-grip-lines-vertical"></i>Spot</p></Link>
-                <nav>
+                {menuToggle ? (
+                <nav className='sm-menu'>
+                    <ul>
+                        {/* 54H */}
+                        <li><Link onClick={()=>setMenuToggle(!menuToggle)} to='/' id='active'>home</Link></li>
+                        <li><Link onClick={()=>setMenuToggle(!menuToggle)} to='/about'>about</Link></li>
+                        <li><Link onClick={()=>setMenuToggle(!menuToggle)} to='/team'>team</Link></li>
+                        <li><Link onClick={()=>setMenuToggle(!menuToggle)} to='/services'>services</Link></li>
+                        <li>Pages<i className='fas fa-sort-down'></i></li>
+                        <li><Link onClick={()=>setMenuToggle(!menuToggle)} to='/blog'>blog</Link></li>
+                        <button className='primary-btn'>submit project</button>
+                    </ul>
+                </nav>):(
+                <nav className='lg-menu'>
                     <ul>
                         {/* 54H */}
                         <li><Link to='/' id='active'>home</Link></li>
@@ -17,8 +33,8 @@ function Navbar() {
                         <li><Link to='/blog'>blog</Link></li>
                         <button className='primary-btn'>submit project</button>
                     </ul>
-                </nav>
-                <i className="menu-icon fas fa-ellipsis-h"></i>
+                </nav>)}
+                <i onClick={()=>setMenuToggle(!menuToggle)}className="menu-icon fas fa-ellipsis-h"></i>
             </div>
 		</header>
 	);
